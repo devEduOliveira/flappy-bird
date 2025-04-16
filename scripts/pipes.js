d1b582c02ff1f5580ee21e7290e3d38561f2cbf8
devEduOliveira <eduardooliveira.tecn@gmail.com>
2025-04-16 14:29:48 -0300

    feat: gerar pipes aleatorios,  sincronia background & pipes

--- scripts/pipes.js
+++ scripts/pipes.js
@@ -3,17 +3,18 @@ const gameBoard = document.querySelector('.gameBoard')
 
 let gameState = {
 
-    positionStart: 0,
+    positionStart: -100,
     loopGeneratePipe: true,
     pipesArray: [],
 
-    gameStarted: false
+    gameStarted: false,
+    tickSpeed: 2000
 }
 
 function drawPipeLoop(){
     setInterval(() => {
         createNewPipe()
-    }, 2500);
+    }, gameState.tickSpeed);
 }
 
 function createElement(element, classList){
@@ -26,7 +27,7 @@ function movePipes() {
     if(!gameState.loopGeneratePipe) return;
 
     gameState.pipesArray.forEach((pipeObj, index) => {
-        pipeObj.position += 2;
+        pipeObj.position += 2.49;
         pipeObj.element.style.right = pipeObj.position + "px"
 
         if(pipeObj.position >= 500){
@@ -46,16 +47,55 @@ function createNewPipe() {
     gameState.pipesArray.push({element: pipe, position: gameState.positionStart})
 }
 
-function createPipeGroup(){
-    const pipe = createElement("div", "pipe")
-    
-    const pipeTop = createPipe("top")
-    const pipeBottom = createPipe("bottom")
-    
-    pipe.appendChild(pipeTop);
-    pipe.appendChild(pipeBottom);
-    return pipe;
+function createPipeGroup() {
+    const pipeGroup = createElement("div", "pipe");
+
+    const pipeTop = createPipe("top");
+    const pipeBottom = createPipe("bottom");
+
+    const { heightTop, heightBase } = getRandomHeights();
+
+    setPipeHeight(pipeTop, heightTop);
+    setPipeHeight(pipeBottom, heightBase);
+
+    positionPipe(pipeTop, "top");
+    positionPipe(pipeBottom, "bottom");
+
+    pipeGroup.appendChild(pipeTop);
+    pipeGroup.appendChild(pipeBottom);
+
+    return pipeGroup;
 }
+
+function getRandomHeights() {
+    const heightMax = 400;
+    const heightMin = 90;
+    const spaceBetween = 220;
+
+    const heightTop = Math.floor(Math.random() * (heightMax - heightMin)) + heightMin;
+    const heightBase = 580 - heightTop - spaceBetween;
+
+    return { heightTop, heightBase };
+}
+
+function setPipeHeight(pipe, height) {
+    const body = pipe.querySelector(".body");
+    if (body) {
+        body.style.height = height + "px";
+    }
+}
+
+function positionPipe(pipe, type) {
+    pipe.style.position = "absolute";
+
+    if (type === "top") {
+        pipe.style.top = "0";
+        pipe.style.transform = "rotate(180deg)";
+    } else {
+        pipe.style.bottom = "90px";
+    }
+}
+
 
 function createPipe(position){
     const pipe = createElement("div", `cano ${position}`)        
@@ -68,12 +108,14 @@ function createPipe(position){
 }
 
 function startGame(event){
-    if(!gameState.gameStarted && 
+    if(!gameState.gameStarted && (
         event.key === "" || 
-        event.code === "Space"
+        event.code === "Space")
     ){
 
         gameState.gameStarted = true;
+        console.log(gameState.gameStarted);
+        
         drawPipeLoop()
     }
 }

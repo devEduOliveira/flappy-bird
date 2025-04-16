20e2c7eb03b7413061aedf4fc075d68085a0f32f
devEduOliveira <eduardooliveira.tecn@gmail.com>
2025-04-16 12:55:36 -0300

    feat: adicao da logica de geração de obstaculos

---
+++ scripts/pipes.js
@@ -0,0 +1,72 @@
+
+const gameBoard = document.querySelector('.gameBoard')
+let positionStart = 0;
+let ativo = true;
+let pipesArray = []; // vamos salvar aqui
+
+function drawPipe(){
+    setInterval(() => {
+        createNewPipe()
+    }, 2500);
+}
+
+function createElement(element, classList){
+    const card = document.createElement(element)
+    card.classList = classList;
+    return card;
+}
+
+function movePipes() {
+    if(!ativo) return;
+
+    pipesArray.forEach((pipeObj, index) => {
+        pipeObj.position += 2;
+        pipeObj.element.style.right = pipeObj.position + "px"
+
+        if(pipeObj.position >= 500){
+            gameBoard.removeChild(pipeObj.element)
+            pipesArray.splice(index, 1)
+        }
+    });
+
+    requestAnimationFrame(movePipes)
+}
+
+function createNewPipe() {
+    const pipe = createPipeGroup()
+    pipe.style.right = positionStart + "px"
+    gameBoard.appendChild(pipe)
+
+    pipesArray.push({element: pipe, position: positionStart})
+}
+
+function createPipeGroup(){
+    const pipe = createElement("div", "pipe")
+    
+    const pipeTop = createPipe("top")
+    const pipeBottom = createPipe("bottom")
+    
+    pipe.appendChild(pipeTop);
+    pipe.appendChild(pipeBottom);
+    return pipe;
+}
+
+function createPipe(position){
+    const pipe = createElement("div", `cano ${position}`)        
+    const pipeHead = createElement("div", "head")
+    const pipeBody = createElement("div", "body")
+
+    pipe.appendChild(pipeHead)
+    pipe.appendChild(pipeBody) 
+    return pipe;
+}
+
+drawPipe()
+movePipes()
+
+
+
+
+
+
+

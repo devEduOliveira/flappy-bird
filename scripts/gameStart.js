af7e73d6dde5348a690af27dd202e26f64f00d49
devEduOliveira <eduardooliveira.tecn@gmail.com>
2025-04-22 15:47:27 -0300

    feat: adicionando tela de inicio

---
+++ scripts/gameStart.js
@@ -0,0 +1,39 @@
+import { createElement, gameState, resetGame } from "./global.js";
+const game = document.querySelector(".game")
+
+function createGameStart(){
+    let modalGameStart = createElement("div", "modalGameStart")
+
+    let gameoverTitle = createElement("h4", "title")
+    gameoverTitle.textContent = `Press "space" to start game`
+
+    modalGameStart.appendChild(gameoverTitle)
+
+    game.appendChild(modalGameStart)
+}
+createGameStart()
+
+
+export function showGameStart(boolean){
+    document.querySelector(".modalGameStart").classList.toggle("hidden", boolean)
+    gameState.gameStartPage = !boolean;
+}
+
+// window.addEventListener("keydown", closeModal)
+
+// function closeModal(event){
+
+//     if(gameState.gameOver && (event.code == "Space" || event.key == "")){
+//         let inputname = document.querySelector(".insertNickname")
+        
+//         if (document.activeElement === inputname) {
+//             resetGame()
+//             inputname.value = ""
+//         } else {
+//             inputname.focus()
+//         }
+        
+//     }
+
+// }
+
